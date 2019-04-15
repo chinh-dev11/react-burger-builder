214e7c812b024f5954076db4508dcef8548dec2b
Chinh Le <chinh.le11@gmail.com>
2019-04-15 19:18:30 -0400

    adding SideDrawer toggle button and hamburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     };
 
     sideDrawerClosedHandler = () => {
@@ -15,10 +15,21 @@ class Layout extends Component {
         });
     };
 
+    sideDrawerToggleHandler = () => {
+        /* this.setState({
+            showSideDrawer: !this.state.showSideDrawer //! getting the state (this.state.showSideDrawer) in setState might not return the previous state due to the async nature of setState, therefore it's recommended to use the function form to get the previous state instead, as below...
+        }); */
+        this.setState((prevState) => {
+            return {
+                showSideDrawer: !prevState.showSideDrawer
+            };
+        });
+    };
+
     render(props) {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
