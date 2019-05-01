e72d2e5330b86ad57dea1177ec25a4a804182652
Chinh Le <chinh.le11@gmail.com>
2019-05-01 08:51:10 -0400

    outputting orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,44 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-);
+const order = (props) => {
+    // console.log(typeof props.price); // price already converted to number in Checkout before posting it to server
+    // console.log(props.ingredients);
+
+    let ingredients = [];
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+    // console.log(ingredients);
+
+    const ingredientsOuput = ingredients.map(ingredient => {
+        return (
+            <span
+                key={ingredient.name}
+                style={{
+                    display: 'inline-block',
+                    textTransform: 'capitalize',
+                    border: '1px solid #ccc',
+                    padding: '5px',
+                    margin: '0 2px'
+                }}>
+                {ingredient.name} ({ingredient.amount})
+            </span>
+        );
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOuput}</p>
+            <p>Price: <strong>${props.price.toFixed(2)}</strong></p>
+            {/* REM: Use Number.parseFloat {Number.parseFloat(props.price).toFixed(2)} to convert price string to a number as below */}
+            {/* REM: Or add plus sign (+) to the price in Orders.js before passing it */}
+            {/* <p>Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong></p> */}
+        </div>
+    );
+}
 
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -12,7 +12,7 @@ class Orders extends Component {
     componentDidMount() {
         axios.get('/orders.json')
             .then(res => {
-                console.log(res.data);
+                // console.log(res.data);
                 const fetchedOrders = [];
                 for (let key in res.data) {
                     fetchedOrders.push({
@@ -20,8 +20,8 @@ class Orders extends Component {
                         id: key
                     });
                 }
-                console.log(fetchedOrders);
-                this.setState({ 
+                // console.log(fetchedOrders);
+                this.setState({
                     loading: false,
                     orders: fetchedOrders
                 });
@@ -34,8 +34,15 @@ class Orders extends Component {
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => {
+                    return (
+                        <Order
+                            key={order.id}
+                            ingredients={order.ingredients}
+                            price={order.price}
+                        />
+                    )
+                })}
             </div>
         );
     }
