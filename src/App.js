fe181ee2e9f87d2181402cfa06fce7c223c48863
Chinh Le <chinh.le11@gmail.com>
2019-05-28 15:16:24 -0400

    19-adding lazy loading with custom hoc/asyncComponent

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import * as actions from './store/actions/index';
 import Spinner from './components/UI/Spinner/Spinner';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
-const lazyLoading = {
+const Async = {
+  Auth: asyncComponent(() => {
+    return import('./containers/Auth/Auth');
+  }),
+  Orders: asyncComponent(() => {
+    return import('./containers/Orders/Orders');
+  }),
+  Checkout: asyncComponent(() => {
+    return import('./containers/Checkout/Checkout');
+  }),
+  Logout: asyncComponent(() => {
+    return import('./containers/Auth/Logout/Logout');
+  })
+};
+
+/* const lazyLoading = {
   Auth: React.lazy(() => import('./containers/Auth/Auth')),
   Orders: React.lazy(() => import('./containers/Orders/Orders')),
   Checkout: React.lazy(() => import('./containers/Checkout/Checkout')),
   Logout: React.lazy(() => import('./containers/Auth/Logout/Logout'))
-};
+}; */
 
 class App extends Component {
   // test purposes: removing interceptors registered in withErrorHandler
@@ -29,7 +45,8 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Auth /></Suspense>} />
+        {/* <Route path="/auth" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Auth /></Suspense>} /> */}
+        <Route path="/auth" component={Async.Auth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
@@ -38,10 +55,14 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes =
         (<Switch>
-          <Route path="/orders" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Orders /></Suspense>} />
+          {/* <Route path="/orders" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Orders /></Suspense>} />
           <Route path="/checkout" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Checkout /></Suspense>} />
           <Route path="/logout" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Logout /></Suspense>} />
-          <Route path="/auth" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Auth /></Suspense>} />
+          <Route path="/auth" render={() => <Suspense fallback={<Spinner />}><lazyLoading.Auth /></Suspense>} /> */}
+          <Route path="/orders" component={Async.Orders} />
+          <Route path="/checkout" component={Async.Checkout} />
+          <Route path="/logout" component={Async.Logout} />
+          <Route path="/auth" component={Async.Auth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to='/' />
         </Switch>
