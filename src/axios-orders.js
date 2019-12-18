b72feadeb0c0606bc0d90029b8f1b6e87c42d35d
Chinh Le <chinh.le11@gmail.com>
2019-12-18 09:35:07 -0500

    removed console

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://react-burger-builder-f2419.firebaseio.com'
     baseURL: 'https://burger-builder-react-8801d.firebaseio.com/'
 });
 
